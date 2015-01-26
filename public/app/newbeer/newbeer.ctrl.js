df5d1ee24988e4ce747dd0cd1f662e8d8121d613
Owen Cowdry <owen@niceagency.co.uk>
2015-01-26 19:01:11 +0000

    Removing console.log

--- public/app/newbeer/newbeer.ctrl.js
+++ public/app/newbeer/newbeer.ctrl.js
@@ -8,14 +8,7 @@
 		var vm = this;
 		vm.newBeer = {};
 
-		// console.log( 'NewBeerCtrl ------------------' );
-		// console.log( 'vm ', vm );
-
 		$scope.$on( 'postBeer', function ( evt, args ) {
-			// console.log( 'evt ', evt );
-			// console.log( 'args ', args );
-			// console.log( 'newBeer ', vm.newBeer );
-			
 			// Add new beer to the DB
 			beerCollectionService.postBeer( vm.newBeer );
 			// Switch back to the main page
