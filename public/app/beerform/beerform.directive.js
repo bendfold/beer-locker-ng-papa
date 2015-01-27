ef4896f56e250a2bc52080f654d6949d51d701d4
Owen Cowdry <owencowdry@gmail.com>
2015-01-27 23:04:36 +0000

    Got image file input working from end, needs tidying up and need to work out how to pass this data to the server. But the test worked :)

--- public/app/beerform/beerform.directive.js
+++ public/app/beerform/beerform.directive.js
@@ -20,6 +20,45 @@ function beerForm() {
 
 	function linkFunc(scope, el, attr, ctrl) {
 		scope.vm.submitAction = attr.submitAction;
+
+// TODO - test image uploader
+
+			var fileinput = document.getElementById( 'fileInput' );
+			var fileDisplayArea = document.getElementById( 'fileDisplayArea' );
+			
+			console.log('fileinput ', fileinput);
+			console.log('fileDisplayArea ', fileDisplayArea);
+
+			fileInput.addEventListener( 'change', function ( e ) {
+				
+				var file = fileInput.files[0];
+				var imageType = /image.*/;
+
+				if ( file.type.match( imageType ) ) {
+					
+					var reader = new FileReader();
+					reader.onload = function ( e ) {
+						console.log( 'reader e ', e );
+						fileDisplayArea.innerHtml = "";
+						
+						var img = new Image();
+						
+						console.log( 'reader ', reader );
+
+						img.src = reader.result;
+
+						fileDisplayArea.appendChild( img );
+					}
+					reader.readAsDataURL( file );
+				} else {
+					fileDisplayArea.innerHtml = 'File not supported!';
+				}
+
+
+			});
+// TODO - test image uploader
+
+
 	}
 }
 
