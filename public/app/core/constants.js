4ca190d61fdc6a6e60667335cc95149745609f84
Owen Cowdry <owencowdry@gmail.com>
2015-03-10 23:19:01 +0000

    Got server & DB hooked up on heroku, but loads of CSS is wonky still also need to dynamically change the protocol on the images depending on environment

--- public/app/core/constants.js
+++ public/app/core/constants.js
@@ -1,9 +1,5 @@
 (function(){
 	'use strict';
-	// TODO - NEED TO GET URL AS ENVIRONMENT VAR TO TO THIS IN BOTH PLACES, DONT HARDCODE localhost:5000 you DIV!
-	
-	// console.log( 'URL ', window.location.origin );
-
 	angular
 		.module( 'app.core' )
 		.constant( 'DB_URL', window.location.origin + '/api/beers' );
