f9c139406a867abecfac0e21959f3d1a8f283d63
Owen Cowdry <owencowdry@gmail.com>
2015-04-16 19:07:36 +0100

    fixed up localBeer model so that the user inoput is not registered until th submit button is fired

--- public/app/beerform/beerform.directive.js
+++ public/app/beerform/beerform.directive.js
@@ -19,60 +19,34 @@ function beerForm() {
 	return directive;
 
 	function linkFunc(scope, el, attr, ctrl) {
-		scope.vm.submitAction = attr.submitAction;
-		
-// TODO >>>>
-		
-		
-
-		// scope[attr.fooModel] = angular.copy(scope.barget);
-		console.log( 'submit ', el[0].querySelector( '.btn-submit' ) );
-		console.log( 'submit ', el );
 		var submitBtn = el[0].querySelector( '.btn-submit' );
+		
+		scope.vm.submitAction = attr.submitAction;
 
 		submitBtn.addEventListener('click', function() {
-			console.log( 'submit was clicked' );
-			console.log( 'scope localbeer ', scope.localBeer.name );
-			console.log( 'scope model ', scope.model );
-			
-			scope.model.name = scope.localBeer.name;
+			var localBeer = scope.localBeer;
+			for (var key in scope.localBeer) {
+				if ( scope.localBeer.hasOwnProperty(key) ) {
+					console.log( 'localbeer key' , key  );
+					console.log( 'localbeer key' , scope.localBeer[key]  );
 
+					var localKey = key,
+						localVal = scope.localBeer[key];
+					
+					scope.model[localKey] = localVal;
+
+				}
+			}
 		}, false);
-
-		// el.on('click', function() {
-		// 	console.log( 'scope localbeer ', scope.localBeer );
-		// 	console.log( 'scope localbeer ', scope.model );
-		// 	angular.forEach( scope.model, function(value, key) {
-		// 		console.log( 'value ', value );
-		// 		console.log( 'key ', key );
-		// 	});
-		// });
-
-		// function changeCheck() {
-
-		// } 
-
-		console.log( 'el ', el );
-		console.log( 'attr ', attr );
-		console.log( 'attr ', attr.model );
-		console.log( 'ctrl ', ctrl );
-
-// TODO >>>>
-
-
-
 	
-	};
+	}
 }
 
 BeerFormListCtrl.$inject = ['$scope', 'beerCollectionService', 'dataservice'];
 
 function BeerFormListCtrl( $scope, beerCollectionService, dataservice ) {
 	var vm = this;
-
-	console.log( '$scope ', $scope );
-};
-
+}
 
 
 
